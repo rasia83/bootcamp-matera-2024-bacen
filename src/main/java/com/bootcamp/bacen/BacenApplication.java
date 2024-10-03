ae700088f6eaf2367fc3c3bbb72b8ed0b79ee207
Tiago Edgar Rasia <103585675+rasia83@users.noreply.github.com>
2024-10-03 16:57:46 -0300

    add start.spring.io files

---
+++ src/main/java/com/bootcamp/bacen/BacenApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.bacen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacenApplication.class, args);
+	}
+
+}
